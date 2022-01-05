5255e8762494d02a57bd9a0ba1a56abeba32ec94
Eduard-Konovka <ed098ua@gmail.com>
2022-01-05 06:53:48 +0200

    Adds logo

--- src/components/ImageGallery/ImagesIdleView.jsx
+++ src/components/ImageGallery/ImagesIdleView.jsx
@@ -1,9 +1,10 @@
 import pendingImage from './phones-tablet-2.png';
+import s from './ImageGallery.module.css';
 
 export default function ImagesIdleView() {
   return (
-    <div role="alert">
-      <img src={pendingImage} alt="Pixabay" />
+    <div role="alert" className={s.box}>
+      <img src={pendingImage} alt="Pixabay" className={s.img} />
     </div>
   );
 }
